9fbef98efb69fa59a3851155f53da77728dd892e
Adriano Barsali <adrianobarsali@MBP-di-Adriano.lan>
2020-11-10 01:07:06 +0100

    Navigation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+
 import Login from './Containers/Login';
+import Register from './Containers/Register';
+
 const Stack = createStackNavigator();
 const App = () => (
   <NavigationContainer>
     <Stack.Navigator>
       <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Register" component={Register} />
     </Stack.Navigator>
   </NavigationContainer>
 );
--- src/Containers/Login/index.js
+++ src/Containers/Login/index.js
@@ -9,18 +9,27 @@ import {
   ButtonText,
   RegisterText,
 } from './styles';
+import {useNavigation} from '@react-navigation/native';
+import {Pressable} from 'react-native';
+
 import ImagemFundo from '../../Assets/Images/fundo.jpeg';
 
-const Login = () => (
-  <Container>
-    <Logo source={ImagemFundo} />
-    <TextLogin>Faça seu Login</TextLogin>
-    <InputField placeholder="Digite seu Email" />
-    <InputField placeholder="Digite sua Senha" />
-    <ButtonLogin>
-      <ButtonText>Login</ButtonText>
-    </ButtonLogin>
-    <RegisterText>Se você não possui cadastro, Registre-se</RegisterText>
-  </Container>
-);
+const Login = () => {
+  const nav = useNavigation();
+  return (
+    <Container>
+      <Logo source={ImagemFundo} />
+      <TextLogin>Faça seu Login</TextLogin>
+      <InputField placeholder="Digite seu Email" />
+      <InputField placeholder="Digite sua Senha" />
+      <ButtonLogin>
+        <ButtonText>Login</ButtonText>
+      </ButtonLogin>
+      <Pressable onPress={() => nav.navigate('Register')}>
+        <RegisterText>Se você não possui cadastro, Registre-se</RegisterText>
+      </Pressable>
+    </Container>
+  );
+};
+
 export default Login;
