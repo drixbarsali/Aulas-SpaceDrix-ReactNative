ae5bd19c8a114cd397c2fef1678f26efe0e53609
Adriano Barsali <adrianobarsali@MBP-di-Adriano.lan>
2020-12-01 23:37:03 +0100

    Criando Show and hide pra senha

--- src/Components/CustomInput/index.js
+++ src/Components/CustomInput/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Container, InputField} from './styles';
 import Icon from 'react-native-vector-icons/Feather';
+import {Pressable} from 'react-native';
 
 const CustomInput = ({icon, placeholder, isPassword = false, ...rest}) => {
+  const [togglePassword, setToggle] = useState(true);
+
   return (
     <Container>
       <Icon name={icon} size={20} color="black" />
       <InputField
         placeholder={placeholder}
-        secureTextEntry={isPassword}
+        secureTextEntry={togglePassword ? isPassword : false}
         {...rest}
       />
+      {isPassword && (
+        <Pressable onPress={() => setToggle(!togglePassword)}>
+          <Icon
+            name={togglePassword ? 'eye' : 'eye-off'}
+            size={20}
+            color="black"
+          />
+        </Pressable>
+      )}
     </Container>
   );
 };
